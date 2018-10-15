82276bc1db7b8366ee545f341f259cb5380a4e16
Rafaela Karachon <rakarachon@uc.cl>
2018-10-15 01:40:15 -0300

    feat: add router

--- src/urls.py
+++ src/urls.py
@@ -1,14 +1,20 @@
 from django.conf.urls import url, include
+from django.urls import path, re_path
+from django.contrib import admin
+
 from rest_framework import routers
 from posts import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'posts', views.PostViewSet)
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
+    # re_path('api/(?P<version>(v1|v2))/', include('posts.urls'))
 ]

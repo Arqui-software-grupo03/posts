5d59f17e99acf02171cb6b80f43a38a96c07d67d
Rafaela Karachon <rakarachon@uc.cl>
2018-10-15 22:20:43 -0300

    feat: add hashtag view

--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,28 @@
 # from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
 # from posts.serializers import UserSerializer, GroupSerializer
+from rest_framework import viewsets, response
 
 # from rest_framework import generics
-from .models import Posts
-from posts.serializers import PostsSerializer
+from .models import Posts, Hashtag
+from posts.serializers import PostsSerializer, HashtagSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows posts to be viewed or edited.
     """
-    queryset = Posts.objects.all()#.order_by('-date_joined')
+    queryset = Posts.objects.all()
+    lookup_field = 'id'
     serializer_class = PostsSerializer
+
+#necesario?
+class HashtagViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows hastags to be viewed or edited.
+    """
+    queryset = Hashtag.objects.all()
+    serializer_class = HashtagSerializer
+
 
 # class ListPostsView(generics.ListAPIView):
 #     """

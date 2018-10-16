9cca124792d86ce17884af87de1d27720ded6b7c
Rafaela Karachon <rakarachon@uc.cl>
2018-10-15 22:20:19 -0300

    feat: add hashtag serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,12 +1,34 @@
 # from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from .models import Posts
+from .models import Posts, Hashtag
 
+
+class HashtagSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Hashtag
+        fields = ("url", "name")
 
 class PostsSerializer(serializers.HyperlinkedModelSerializer):
+    hashtags = HashtagSerializer(many=True)
+
     class Meta:
         model = Posts
-        fields = ("url","content")
+        fields = ("content", "hashtags")
+
+    def create(self, validated_data):
+        content = validated_data['content'].split(' ')
+        tags = []
+        for term in content:
+            if term[0] == '#':
+                tags.append(Hashtag.objects.create(name=term[1:]))
+
+        post = Posts.objects.create(
+            # url = validated_data['url'],
+            content = validated_data['content'],
+        )
+        post.hashtags.set(tags)
+
+        return post
 
 # class UserSerializer(serializers.HyperlinkedModelSerializer):
 #     class Meta:
